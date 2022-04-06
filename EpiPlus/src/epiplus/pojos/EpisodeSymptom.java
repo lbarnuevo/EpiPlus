98d26a4f3c8cfab43533e9d3bb0527c208aaf1a8
lucky <lucky@10.60.86.36>
2022-04-06 15:19:12 +0200

    POJO for episodesymptom relationship

--- EpiPlus/src/epiplus/pojos/EpisodeSymptom.java
+++ EpiPlus/src/epiplus/pojos/EpisodeSymptom.java
@@ -1,6 +1,66 @@
 package epiplus.pojos;
 
-public class EpisodeSymptom {
+import java.io.Serializable;
+import java.util.Objects;
 
-	//TODO do the same that I did with patientmedication for the many to many relationshio @lbarnuevo 
+public class EpisodeSymptom implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8368812583304410229L;
+	
+	private Integer severity; 
+	private Episode episode;
+	private Symptom symptom;
+	
+	public EpisodeSymptom() {
+		super();
+	}
+
+	public Integer getSeverity() {
+		return severity;
+	}
+
+	public void setSeverity(Integer severity) {
+		this.severity = severity;
+	}
+
+	public Episode getEpisode() {
+		return episode;
+	}
+
+	public void setEpisode(Episode episode) {
+		this.episode = episode;
+	}
+
+	public Symptom getSymptom() {
+		return symptom;
+	}
+
+	public void setSymptom(Symptom symptom) {
+		this.symptom = symptom;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(episode, symptom);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EpisodeSymptom other = (EpisodeSymptom) obj;
+		return Objects.equals(episode, other.episode) && Objects.equals(symptom, other.symptom);
+	}
+
+	@Override
+	public String toString() {
+		return "EpisodeSymptom [severity=" + severity + ", episode=" + episode + ", symptom=" + symptom + "]";
+	}
 }
