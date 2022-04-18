9e417ba740920281cff79a4784efb2cdffe02c6d
lucky <lucky@LAPTOP-FMQEL699.home>
2022-04-18 19:34:42 +0200

    POJO for Emergency Contacts

--- EpiPlus/src/epiplus/pojos/EmergencyContact.java
+++ EpiPlus/src/epiplus/pojos/EmergencyContact.java
@@ -1,7 +1,69 @@
 package epiplus.pojos;
 
+import java.util.Objects;
+
 public class EmergencyContact {
 
-	//TODO EmergencyContact POJO following what rodrigo said in presentation 
-	// 1-to-many relationship with patient 
+	private Integer id;
+	private String name;
+	private Float number;
+	private Patient patient;
+	
+	public EmergencyContact() {
+		super();
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getNumber() {
+		return number;
+	}
+
+	public void setNumber(Float number) {
+		this.number = number;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmergencyContact other = (EmergencyContact) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "EmergencyContact [id=" + id + ", name=" + name + ", number=" + number + ", patient=" + patient + "]";
+	}
 }
