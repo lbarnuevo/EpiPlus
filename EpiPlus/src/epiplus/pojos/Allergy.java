1c1d1ba23f59610fd34e2dacad3368d9d9f2a350
lucky <lucky@LAPTOP-FMQEL699>
2022-05-04 11:21:24 +0200

    Added mandatory constructor

--- EpiPlus/src/epiplus/pojos/Allergy.java
+++ EpiPlus/src/epiplus/pojos/Allergy.java
@@ -4,71 +4,61 @@ import java.io.Serializable;
 import java.util.Objects;
 
 
+
+public class Allergy implements Serializable{
 	
-	public class Allergy implements Serializable{
-		
-		
-	
-		private static final long serialVersionUID = -935621693802822621L;
+	private static final long serialVersionUID = -935621693802822621L;
 
+	private Integer id;
+	private String name;
 
+	public Allergy() {
+		super();
+	}
 
-
-		public Allergy(Integer id, String name) {
-			super();
-			this.id = id;
-			this.name = name;
-		}
-
-		
-		
-		
-		private Integer id;
-		private String name;
-		
-		
-	
+	public Allergy(Integer id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 	
 	public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		@Override
-		public int hashCode() {
-			return Objects.hash(id);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Allergy other = (Allergy) obj;
-			return Objects.equals(id, other.id);
-		}
-
-		@Override
-		public String toString() {
-			return "Allergy [id=" + id + ", name=" + name + "]";
-		}
-
-
-	
+		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allergy other = (Allergy) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Allergy [id=" + id + ", name=" + name + "]";
+	}
+}
 
 
