55edf74c7a5c577e96296468a3c755b7239c643d
lucky <lucky@LAPTOP-FMQEL699.home>
2022-05-16 22:58:52 +0200

    Fixed createTables method

--- EpiPlus/src/epiplus/jdbc/JDBCManager.java
+++ EpiPlus/src/epiplus/jdbc/JDBCManager.java
@@ -13,7 +13,7 @@ public class JDBCManager {
 		try {
 			// Open database connection
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:./db/EpiPlus (3).db");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/epiplus.db");
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
 			// Create tables
@@ -43,72 +43,110 @@ public class JDBCManager {
 		// Create Tables
 		try {
 			Statement stmt = c.createStatement();
+			
 			// -----------------DOCTORS----------
-			String sql = "CREATE TABLE doctors (" + "	id	    INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "	name	TEXT NOT NULL," + "	email	TEXT NOT NULL," + "	hospitalName TEXT NOT NULL,"
-					+ " photo BLOB " + ");";
+			String sql = "CREATE TABLE doctors (" 
+					+ "	id		INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "	name	TEXT NOT NULL," 
+					+ "	email	TEXT NOT NULL," 
+					+ "	hospitalName TEXT NOT NULL,"
+					+ " photo BLOB " 
+					+ ");";
 			stmt.executeUpdate(sql);
 
-			// -----------EPISODES
-			sql = "CREATE TABLE episodes (" + "	id	    INTEGER PRIMARY KEY AUTOINCREMENT," + "	date	DATE NOT NULL,"
-					+ "	length	REAL NOT NULL," // time in minutes
+			// -----------------PATIENTS----------
+			sql = "CREATE TABLE patients(" 
+					+ " id 			INTEGER PRIMARY KEY AUTOINCREMENT," 
+					+ " name		TEXT NOT NULL,"
+					+ " email		TEXT NOT NULL,"
+					+ " birthday 	DATE NOT NULL," 
+					+ " height		REAL," // in cm
+					+ " weight		REAL," // in kg
+					+ " lifestyle 	TEXT CHECK('lifestyle' IN ('sedentary','low activity','medium ativity','high activity')),"
+					+ " diet 		TEXT CHECK ('diet' IN ('normal','mediterranean', 'high protein','high protein vegetarian', 'high protein vegan', 'gluten free', 'lactose free', 'dairy free', 'ketogenic', 'ketogenic vegetarian', 'ketogenic vegan', 'vegan','vegetarian')),"
+					+ " ex_per_week INTEGER,"
+					+ " photo 		BLOB,"
+					+ " doctorId INTEGER NOT NULL REFERENCES doctors(id) ON DELETE SET NULL" 
+					+ ");";
+			
+			// -----------------EPISODES----------
+			sql = "CREATE TABLE episodes (" 
+					+ "	id	    	INTEGER PRIMARY KEY AUTOINCREMENT," 
+					+ "	date		DATE NOT NULL,"
+					+ "	length		REAL NOT NULL," // time in minutes
 					+ "	activity	TEXT," // before attack
-					+ "	mood	    TEXT," + "	place	TEXT," + "	previous_meal TEXT,"
-					+ "	injuries BOOLEAN NOT NULL,"
-					+ "FOREIGN KEY(patientID) REFERENCES patients(id) ON DELETE RESTRICT" + ");";
+					+ "	mood	    TEXT," 
+					+ "	place		TEXT," 
+					+ "	previous_meal TEXT,"
+					+ "	injuries 	BOOLEAN NOT NULL,"
+					+ " patientId INTEGER NOT NULL REFERENCES patients(id) ON DELETE RESTRICT"
+					+ ");";
 			stmt.executeUpdate(sql);
 			
-			// ----------SYMPTOMS
-			sql= "CREATE TABLE symptoms (" + "id	    INTEGER PRIMARY KEY AUTOINCREMENT," + " name	TEXT NOT NULL," + ");";
+			// -----------------SYMPTOMS----------
+			sql= "CREATE TABLE symptoms (" 
+					+ " id	    INTEGER PRIMARY KEY AUTOINCREMENT," 
+					+ " name	TEXT NOT NULL," 
+					+ ");";
 			stmt.executeUpdate(sql);		
 
-			// ---------EPISODE-SYMPTOMS
-			sql = "CREATE TABLE episodesymptoms(" + "	episodeId INTEGER NOT NULL,"
-					+ "	symptomId	INTEGER NOT NULL," + "	severity	INTEGER NOT NULL,"
-					+ " FOREIGN KEY (episodeId) REFERENCES episodes(id) ON DELETE RESTRICT,"
-					+ " FOREIGN KEY (symptomId) REFERENCES symptoms(id) ON DELETE RESTRICT" + ");";
-			stmt.executeUpdate(sql);
-
-			// -------MEDICATIONS
-			sql = "CREATE TABLE medications (" + "	id	 INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,"
-					+ "	name   TEXT NOT NULL" + ");";
-			stmt.executeUpdate(sql);
-
-			sql = "CREATE TABLE patientmedication( " + "patientId INTEGER NOT NULL," + "medicationId INTEGER NOT NULL, "
-					+ "frequency INTEGER NOT NULL," // times per day
-					+ "amount REAL NOT NULL,"
-					+ " FOREIGN KEY (patientId) REFERENCES patients(id) ON DELETE RESTRICT,"
-					+ " FOREIGN KEY (medicationId) REFERENCES medications(id) ON DELETE RESTRICT"
+			// -----------------EPISODE-SYMPTOMS----------
+			sql = "CREATE TABLE episodesymptoms (" 
+					+ "	episodeId 	INTEGER NOT NULL,"
+					+ "	symptomId	INTEGER NOT NULL," 
+					+ "	severity	INTEGER NOT NULL,"
+					+ " FOREIGN KEY (episodeId) REFERENCES episodes(id) ON DELETE CASCADE,"
+					+ " FOREIGN KEY (symptomId) REFERENCES symptoms(id) ON DELETE CASCADE,"
+					+ " PRIMARY KEY (episodeId,symptomId)\r\n"
 					+ ");";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE patients(" + "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + "name TEXT NOT NULL,"
-					+ "email TEXT NOT NULL,"
-					+ "age INTEGER NOT NULL," + "height REAL NOT NULL," // in cm
-					+ "weight REAL NOT NULL, " // in kg
-					+ "lifestyle TEXT CHECK('lifestyle' IN ('sedentary','little activity','moderate ativity','high activity')) NOT NULL,"
-					+ "diet TEXT CHECK ('diet' IN ('normal','mediterranean', 'high protein','high protein vegetarian', 'high protein vegan', 'gluten free', 'lactose free', 'dairy free', 'ketogenic', 'ketogenic vegetarian', 'ketogenic vegan', 'vegan','vegetarian')) NOT NULL,"
-					+ "ex_per_week INTEGER NOT NULL," + "doctorId INTEGER NOT NULL," + "photo BLOB,"
-					+ "FOREIGN KEY (doctorId) REFERENCES doctors(id) ON DELETE SET NULL"
+			// -----------------MEDICATIONS----------
+			sql = "CREATE TABLE medications (" 
+					+ "	id	 	INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,"
+					+ "	name   	TEXT NOT NULL" 
 					+ ");";
-			//I deleted intermitten fasting because it makes things harder
-			//I added normal = not specified
-			// high protein wege and wegan
+			stmt.executeUpdate(sql);
+			
+			// -----------------PATIENT-MEDICATION----------
+			sql = "CREATE TABLE patientmedication( " 
+					+ " patientId 		INTEGER NOT NULL," 
+					+ " medicationId 	INTEGER NOT NULL,"
+					+ " frequency 		INTEGER NOT NULL," // times per day
+					+ " amount 			REAL NOT NULL,"
+					+ " FOREIGN KEY (patientId) REFERENCES patients(id) ON DELETE CASCADE,"
+					+ " FOREIGN KEY (medicationId) REFERENCES medications(id) ON DELETE CASCADE"
+					+ " PRIMARY KEY (patientId,medicationId)\r\n"
+					+ ");";
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE emergencycontact (" + "id	 INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"
-					+ "name	TEXT NOT NULL," + "number	INTEGER NOT NULL," + "FOREIGN KEY (patientId) REFERENCES patients(id) ON DELETE SET NULL);";
+			// -----------------EMERGENCY CONTACT----------
+			sql = "CREATE TABLE emergencycontacts (" 
+					+ " id	 	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"
+					+ " name	TEXT NOT NULL," 
+					+ " number	INTEGER NOT NULL," 
+					+ " patientId INTEGER NOT NULL REFERENCES patients(id) ON DELETE CASCADE"
+					+ ");";
 
 			stmt.executeUpdate(sql);
 			
-			sql = "CREATE TABLE allergies (" + "	id	    INTEGER PRIMARY KEY AUTOINCREMENT,"
-					+ "	name	TEXT NOT NULL);";
+			// -----------------ALLERGY----------
+			sql = "CREATE TABLE allergies (" 
+					+ "	id	    INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "	name	TEXT NOT NULL"
+					+ ");";
 
 			stmt.executeUpdate(sql);
 
-			sql = "CREATE TABLE patientallergies (" + "	FOREING KEY(PatientId) REFERENCES ON patients(id),"
-					+ "	FOREIGN KEY (allergId) REFERENCES ON allergies(id));";
-					
+			// -----------------PATIENT-ALLERGY----------
+			sql = "CREATE TABLE patientallergies ("
+					+ " patientId 		INTEGER NOT NULL,"
+					+ " allergyId		INTEGER NOT NULL,"
+					+ "	FOREING KEY (patientId) REFERENCES patients(id) ON DELETE CASCADE,"
+					+ "	FOREIGN KEY (allergyId) REFERENCES allergies(id) ON DELETE CASCADE,"
+					+ " PRIMARY KEY (patientId,allergyId)\r\n"
+					+ ");";
+	
 			stmt.executeUpdate(sql);
 
 
