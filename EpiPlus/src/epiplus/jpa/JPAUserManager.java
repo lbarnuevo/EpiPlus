904ca537491ca4f5fe60863f8e03cf11fd02667a
lucky <lucky@10.60.83.43>
2022-05-24 13:26:02 +0200

    Added jpa operations

--- EpiPlus/src/epiplus/jpa/JPAUserManager.java
+++ EpiPlus/src/epiplus/jpa/JPAUserManager.java
@@ -55,9 +55,9 @@ public class JPAUserManager implements UserManager {
 	}
 
 	@Override
-	public Role getRole(String name) {
-		Query q = em.createNativeQuery("SELECT * FROM roles WHERE name = ?", Role.class);
-		q.setParameter(1, name);
+	public Role getRole(int id) {
+		Query q = em.createNativeQuery("SELECT * FROM roles WHERE id = ?", Role.class);
+		q.setParameter(1, id);
 		return (Role) q.getSingleResult();
 	}
 
@@ -85,5 +85,63 @@ public class JPAUserManager implements UserManager {
 			u = (User) q.getSingleResult();
 		} catch (NoResultException e) {}
 		return u;
+	}
+	
+	@Override
+	public Boolean checkEmail(String email) {
+		try {
+			Query q = em.createNativeQuery("SELECT * FROM users WHERE email = ?", User.class);
+			q.setParameter(1, email);
+			User temp = (User)q.getSingleResult();
+			if(temp.getEmail().equalsIgnoreCase(email)) {
+				return true;
+			}else {
+				return false;
+			}
+		}catch(NoResultException nre) {
+			return false;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Override
+	public void deleteUser(String email) {
+		Query q = em.createNativeQuery("SELECT * FROM users WHERE email = ?", User.class);
+		q.setParameter(1, email);
+		User u = (User) q.getSingleResult();
+		
+		em.getTransaction().begin();
+		em.remove(u);
+		em.getTransaction().commit();
+	}
+	
+	@Override
+	public Boolean updatePassword(String mail, String newPass, String oldPass) {
+		try {
+ 			MessageDigest md = MessageDigest.getInstance("MD5");
+ 			md.update(oldPass.getBytes());
+ 			byte[] hash = md.digest();
+ 			
+ 			Query q = em.createNativeQuery("SELECT * FROM users WHERE email = ? AND password = ?", User.class);
+ 			q.setParameter(1, mail);
+ 			q.setParameter(2, hash);
+ 			
+ 			User u = (User) q.getSingleResult();
+ 			MessageDigest md2 = MessageDigest.getInstance("MD5");
+ 			md2.update(newPass.getBytes());
+ 			byte[] hash2 = md2.digest();
+
+ 			em.getTransaction().begin();
+ 			u.setPassword(hash2);
+ 			em.getTransaction().commit();
+ 			
+ 		}catch(Exception e) {
+ 			System.out.println("The current password is not correct");
+ 			return true;
+ 		}
+ 		return null;
 	}
 }
