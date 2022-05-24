ba0b968d43bb4564e0c764e07948475cc9557418
lucky <lucky@10.60.83.43>
2022-05-24 13:27:16 +0200

    Fixed mistake in role

--- EpiPlus/src/epiplus/pojos/Role.java
+++ EpiPlus/src/epiplus/pojos/Role.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 import javax.persistence.TableGenerator;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
