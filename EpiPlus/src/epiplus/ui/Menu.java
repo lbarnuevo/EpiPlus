06baf27a607b029dd55afa42e93617bfdae02383
isama <isama@LAPTOP-8VL7NCQ8>
2022-06-05 14:19:33 +0200

    changed minor typo

--- EpiPlus/src/epiplus/ui/Menu.java
+++ EpiPlus/src/epiplus/ui/Menu.java
@@ -1047,7 +1047,7 @@ public class Menu {
 				} else {
 					listDoctors(docs);
 					do {
-						System.out.println("\nIntroduce the doctor´s id: ");
+						System.out.println("\nIntroduce the doctor's id: ");
 						d = doctorManager.getDoctorById(Auxiliar.getPositiveInteger(reader));
 					} while (d == null);
 					return d;
